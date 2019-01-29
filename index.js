2286a11eaa236251a534a26b13eb3ce8359d4b9b
Joe Corwin <joe.corwin@gmail.com>
2019-01-29 23:20:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function getFirstSelector(selector) {
 //   let lastChild = document.getElementById('grand-node').querySelectorAll('div')
 //   return lastChild[lastChild.length - 1]
 // }
-// 
+//
 //  function increaseRankBy(n) {
 //   let ranked = document.querySelectorAll('.ranked-list li')
 //   for (let i = 0; i < ranked.length; i++) {
