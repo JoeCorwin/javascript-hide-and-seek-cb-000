4377cf1b50220252287231cce3fb356eb5f14812
Joe Corwin <joe.corwin@gmail.com>
2019-01-29 23:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function getFirstSelector(selector) {
-  return document.querySelector(selector)
+  let element = document.querySelectorAll(selector)
+  return element[0]
 }
 
-function nestedTarget() {
-  document.getElementById('div.nested')
+ function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')
 }
+
+//  function deepestChild() {
+//   let lastChild = document.getElementById('grand-node').querySelectorAll('div')
+//   return lastChild[lastChild.length - 1]
+// }
+// 
+//  function increaseRankBy(n) {
+//   let ranked = document.querySelectorAll('.ranked-list li')
+//   for (let i = 0; i < ranked.length; i++) {
+//     ranked[i].innerHTML = (parseInt(ranked[i].innerHTML) + n).toString()
+//   }
