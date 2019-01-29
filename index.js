668028bf368e9247dd981ed527e5f9965e190000
Joe Corwin <joe.corwin@gmail.com>
2019-01-29 23:26:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ function getFirstSelector(selector) {
   return element[0]
 }
 
-//  function nestedTarget() {
-//   let element = document.getElementById('nested').getElementsByClassName('target')
-//   return element[0]
-// }
-
-function nestedTarget() {
-  return document.querySelector('div.target')
+ function nestedTarget() {
+  let element = document.getElementById('nested').getElementsByClassName('target')
+  return element[0]
 }
 
-//  function deepestChild() {
-//   let lastChild = document.getElementById('grand-node').querySelectorAll('div')
-//   return lastChild[lastChild.length - 1]
-// }
-//
-//  function increaseRankBy(n) {
-//   let ranked = document.querySelectorAll('.ranked-list li')
-//   for (let i = 0; i < ranked.length; i++) {
-//     ranked[i].innerHTML = (parseInt(ranked[i].innerHTML) + n).toString()
-//   }
+
+ function deepestChild() {
+  let lastChild = document.getElementById('grand-node').querySelectorAll('div')
+  return lastChild[lastChild.length - 1]
+}
+
+ function increaseRankBy(n) {
+  let ranked = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < ranked.length; i++) {
+    ranked[i].innerHTML = (parseInt(ranked[i].innerHTML) + n).toString()
+  }
